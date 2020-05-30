89937b13ba7ff165cb593f4bb6eefebe9aa824f6
John Fawcett <43934323+jwfawcett@users.noreply.github.com>
2020-05-30 03:22:59 -0400

    Add files via upload

---
+++ PossiblePrivelegeEscalations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+CurrentFiles= ls -la 
+
+
+
+#looks at the files and permissions in current directory
+#cho "Your Current Directory Shows:" 
+$CurrentFiles
+
+echo "And"
+
+#Pulls all Setuid executable files with root permissions
+echo "Your Root SUID Executables"
+
+ find / -user root -perm -4000 -print 
+
+
+
+
+
+
+
+  
