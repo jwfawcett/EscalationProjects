d371c58da65e38055961b12cf807a7e3dab4b5f8
John Fawcett <43934323+jwfawcett@users.noreply.github.com>
2021-03-30 12:20:20 -0400

    Update seewritable.sh

--- seewritable.sh
+++ seewritable.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
+
+# gets writable files on machine and puts it in targets text file 
 find / -writable -type d 2>/dev/null > targets.txt
+
+#gets the latest syslog entries
+tail /var/log/syslog 
